d52114fb034f2bf32492b8e32d9b4368707a1f5d
dremansley1 <mansleyat@cardiff.ac.uk>
2020-02-06 22:19:50 +0000

    Design and Layout

--- museum/routes.py
+++ museum/routes.py
@@ -27,36 +27,34 @@ def room_artifacts():
     return render_template('room_artifacts.html', museum_data = museum_info, page_name = "Artifacts");
 
 
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST':
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user)
+            flash("You have been signed in!")
+            return redirect(url_for('mainPage'))
+        else:
+            flash("Incorrect Login Credentials!")
+            return redirect(url_for('login'))
+    return render_template('login.html', page_name='Login to Your Account', museum_data = museum_info, form=form)
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data,
+        email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("You have been successfully registered") 
+        return redirect(url_for('login'))
+    return render_template('register.html', page_name='Create an Account', museum_data = museum_info, form=form)
 
-#@app.route("/login", methods=['GET', 'POST'])
-#def login():
-#    form = LoginForm()
-#    if request.method == 'POST':
-#        user = User.query.filter_by(email=form.email.data).first()
-#        if user is not None and user.verify_password(form.password.data):
-#            login_user(user)
-#            flash("You have been signed in!")
-#            return redirect(url_for('mainPage'))
-#        else:
-#            flash("Incorrect Login Credentials!")
-#            return redirect(url_for('login'))
-#    return render_template('login.html',  store_data = museum_info, page_name='Login to Your Account', form=form)
-#
-#@app.route("/register", methods=['GET', 'POST'])
-#def register():
-#    form = RegistrationForm()
-#    if form.validate_on_submit():
-#        user = User(username=form.username.data,
-#        email=form.email.data, password=form.password.data)
-#        db.session.add(user)
-#        db.session.commit()
-#        flash("You have been successfully registered") 
-#        return redirect(url_for('login'))
-#    return render_template('register.html', page_name='Create an Account', museum_data = museum_info, form=form)
-#
-#@app.route("/logout")
-#def logout():
-#    logout_user()
-#    flash("You have been logged out successfully")
-#    return redirect(url_for('mainPage'))
+@app.route("/logout")
+def logout():
+    logout_user()
+    flash("You have been logged out successfully")
+    return redirect(url_for('mainPage'))
